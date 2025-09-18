92678bd7298596bd7c9d70b377375e7f34edc2ba
Robert9710 <cireasarobert@yahoo.com>
2025-09-18 10:47:47 +0100

    Added topic tree component on the home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
 import TopicBlocks from "../../components/TopicBlocks/TopicBlocks";
+import TopicTree from "../../components/TopicTree/TopicTree";
 
 export default function Home() {
   return (
     <div id="home" className="row">
-      <div className="col-3"></div>
+      <div className="col-3">
+        <TopicTree />
+      </div>
       <div className="col-6">
         <TopicBlocks />
       </div>
